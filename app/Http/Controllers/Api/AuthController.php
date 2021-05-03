a08833235eecd70e8fef6c9eb0d3a57c86b1be4f
fathin123 <fathinnaufalia@gmail.com>
2021-05-04 02:34:08 +0700

    Praktikum1_PembuatanFiturRegisterdanLogin

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use app\Traits;
+
+class AuthController extends Controller
+{
+    //
+    use ApiResponse;
+
+    public function login(LoginRequest $request)
+    {
+        $validated = $request->validated();
+
+        if (!Auth::attempt($validated)) {
+            return $this->apiError('Credentials not match', Response::HTTP_UNAUTHORIZED);
+        }
+
+        $user = User::where('email', $validated['email'])->first();
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return $this->apiSuccess([
+            'token' => $token,
+            'token_type' => 'Bearer',
+            'user' => $user,
+        ]);
+    }
+}
