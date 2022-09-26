c313fea0ae87e986275b6bef315d3395273ad65b
fathin123 <fathinnaufalia@gmail.com>
2022-09-26 09:55:59 +0700

    update regis&login

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -11,6 +11,23 @@ class AuthController extends Controller
     //
     use ApiResponse;
 
+    public function register(RegisterRequest $request)
+    {
+        $validated = $request->validated();
+        $user = User::create([
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+            'password' => Hash::make($validated['password']),
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+        return $this->apiSuccess([
+            'token' =>$token,
+            'token_type' => 'Bearer',
+            'user' => $user,
+        ]);
+    }
+
     public function login(LoginRequest $request)
     {
         $validated = $request->validated();
